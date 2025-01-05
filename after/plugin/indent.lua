af2ffdc6c6cbee0feeac2efbaa641c4f58ac2ba9
therealtimhawkins <timothy.hawkins@novata.com>
2025-01-05 13:05:34 +0000

    fixes indent scope color

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -1,12 +1,12 @@
 local highlight = {
-	"RainbowBlue",
+	"RainbowCyan",
 }
 local hooks = require("ibl.hooks")
-local colors = require("dracula").colors()
-
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = colors.blue })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#8be9fd" })
 end)
 
 vim.g.rainbow_delimiters = { highlight = highlight }
 require("ibl").setup({ scope = { highlight = highlight } })
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)

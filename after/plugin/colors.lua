2582e3e3b0daf46365f7fc954fcfe8afa9d06109
therealtimhawkins <timothy.hawkins@novata.com>
2025-01-05 11:36:44 +0000

    adds color highlight for numbers

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,3 +3,38 @@ dracula.setup({
 	transparent_bg = true,
 })
 vim.cmd([[colorscheme dracula]])
+
+vim.opt.cursorline = true
+vim.cmd([[highlight CursorLineNr guifg=#8be9fd	]])
+
+-- Set default color for normal mode
+vim.cmd([[highlight CursorLineNr guifg=#ff79c6	]])
+
+-- Change color for different modes
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*:[vV\x16]*",
+	callback = function()
+		vim.cmd([[highlight CursorLineNr guifg=#bd93f9	]])
+	end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*:t*",
+	callback = function()
+		vim.cmd([[highlight CursorLineNr guifg=#ffb86c	]])
+	end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*:i*",
+	callback = function()
+		vim.cmd([[highlight CursorLineNr guifg=#8be9fd	]])
+	end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "[vV\x16ti]*:*",
+	callback = function()
+		vim.cmd([[highlight CursorLineNr guifg=#ff79c6	]])
+	end,
+})
--- lua/therealtimhawkins/init.lua
+++ lua/therealtimhawkins/init.lua
@@ -12,11 +12,3 @@ vim.opt.foldlevel = 99
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "typescriptreact" },
-	callback = function()
-		vim.opt_local.includeexpr = "substitute(v:fname, '^@/', '', '')"
-		vim.opt_local.suffixesadd:append(".ts,.tsx,.d.ts")
-	end,
-})

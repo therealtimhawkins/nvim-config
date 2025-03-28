56098473f1d9c0eb00b279d37e5b8f98f608a862
therealtimhawkins <timothy.hawkins@novata.com>
2025-03-28 12:14:10 +0000

    opens README by default when opening neovim

--- lua/therealtimhawkins/init.lua
+++ lua/therealtimhawkins/init.lua
@@ -1,6 +1,31 @@
 require("therealtimhawkins.remap")
 print("realtimhawkins loaded")
 
+-- Function to open README file if it exists when opening a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+			local readme_patterns = {
+				"README.md",
+				"README.txt",
+				"README",
+				"Readme.md",
+				"readme.md",
+			}
+
+			for _, pattern in ipairs(readme_patterns) do
+				local readme_path = vim.fn.argv(0) .. "/" .. pattern
+				if vim.fn.filereadable(readme_path) == 1 then
+					-- Open the README file
+					vim.cmd("edit " .. vim.fn.fnameescape(readme_path))
+					break
+				end
+			end
+		end
+	end,
+	pattern = "*",
+})
+
 vim.opt.path:append("src")
 vim.opt.path:append("node_modules")
 

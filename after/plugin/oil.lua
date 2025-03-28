df274c4f29431c9e3f7e481e83d18c69d33c8577
therealtimhawkins <timothy.hawkins@novata.com>
2025-03-28 12:11:21 +0000

    makes yazi default file explorer

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,3 +1,5 @@
-require("oil").setup()
+require("oil").setup({
+	default_file_explorer = false,
+})
 
 vim.keymap.set("n", "go", "<CMD>Oil --float %:p:h<CR>", { desc = "Open directory" })
--- after/plugin/yazi.lua
+++ after/plugin/yazi.lua
@@ -1,16 +1,13 @@
 require("yazi").setup({
-	-- if you want to open yazi instead of netrw, see below for more info
-	open_for_directories = false,
 	keymaps = {
 		show_help = "<f1>",
 	},
+	open_for_directories = true,
+	init = function()
+		vim.g.loaded_netrwPlugin = 1
+	end,
 })
 
--- Key mappings
 vim.keymap.set("n", "<leader>-", "<cmd>Yazi<cr>", { desc = "Open yazi at the current file" })
-
 vim.keymap.set("n", "<leader>cw", "<cmd>Yazi cwd<cr>", { desc = "Open the file manager in nvim's working directory" })
-
--- NOTE: this requires a version of yazi that includes
--- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
 vim.keymap.set("n", "<c-up>", "<cmd>Yazi toggle<cr>", { desc = "Resume the last yazi session" })

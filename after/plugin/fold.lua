1bd8c909a781e8f26ffc2f8be67df50c4d69524c
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2025-07-29 09:09:27 +0100

    updates folded code style

--- after/plugin/fold.lua
+++ after/plugin/fold.lua
@@ -1,3 +1,5 @@
+local colors = require("dracula").colors()
+
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = true
@@ -6,3 +8,8 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldcolumn = "1"
 
 vim.keymap.set('n', '<leader>z', 'za', { desc = 'Toggle fold' })
+
+vim.api.nvim_set_hl(0, "Folded", {
+	bg = colors.visual,
+	fg = colors.foreground
+})
